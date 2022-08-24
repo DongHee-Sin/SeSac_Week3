d63600d6178b3b66e924febe47430ab1ed2701e7
DongHee-Sin <voslr0601@naver.com>
2022-08-24 19:33:07 +0900

    쇼핑 상세 페이지로 이동할 때, 이미지도 띄우도록 수정

--- SeSac3TrendMedia/Shopping/Shopping/ShoppingDetailViewController.swift
+++ SeSac3TrendMedia/Shopping/Shopping/ShoppingDetailViewController.swift
@@ -14,6 +14,8 @@ class ShoppingDetailViewController: UIViewController {
     @IBOutlet weak var isFinishButton: UIButton!
     @IBOutlet weak var isImportantButton: UIButton!
     
+    @IBOutlet weak var shoppingImageView: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,5 +30,19 @@ class ShoppingDetailViewController: UIViewController {
         navigationItem.title = data.title
         isFinishButton.setImage(data.isFinish ? UIImage(systemName: "checkmark.square.fill") : UIImage(systemName: "checkmark.square"), for: .normal)
         isImportantButton.setImage(data.isImportant ? UIImage(systemName: "star.fill") : UIImage(systemName: "star"), for: .normal)
+        
+        loadImageFromDocument(fileName: "\(data.objectId)")
+    }
+    
+    
+    func loadImageFromDocument(fileName: String) {
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
+        let fileURL = documentDirectory.appendingPathComponent("\(fileName).jpg")
+        
+        if FileManager.default.fileExists(atPath: fileURL.path) {
+            shoppingImageView.image = UIImage(contentsOfFile: fileURL.path)
+        }else {
+            shoppingImageView.image = UIImage(systemName: "star.fill")
+        }
     }
 }
